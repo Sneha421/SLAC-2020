051f13c2f5fa4f48a9306d0cff01a46f90f0ecb5
Suneel Nadipalli <nsuneel89@gmail.com>
2020-11-08 00:00:02 +0530

    Added api to fetch form data
    Signed-off-by: Suneel Nadipalli <nsuneel89@gmail.com>

--- slac_Backend/src/main/java/com/example/demo/formController.java
+++ slac_Backend/src/main/java/com/example/demo/formController.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -23,32 +22,60 @@ public class formController
         }
     }
 
+
+    @GetMapping(path = "/getForm")
+    public @ResponseBody List<formModel> getForm() throws SQLException {
+        List<formModel> formDataList = new ArrayList<>();
+        String getQuery = "SELECT * FROM patientInfo";
+        Statement getStatement = connection.createStatement();
+        ResultSet formDataResult = getStatement.executeQuery(getQuery);
+        while (formDataResult.next())
+        {
+            formModel formModel = new formModel();
+
+            formModel.setParamedicName(formDataResult.getString("paramedicName"));
+            formModel.setPatientDesc(formDataResult.getString("patientDesc"));
+            formModel.setPatientAge(formDataResult.getInt("patientAge"));
+            formModel.setPatientConsc(formDataResult.getString("patientConsc"));
+            formModel.setPatientGender(formDataResult.getString("patientGender"));
+            formModel.setAccidentCategory(formDataResult.getString("accidentCategory"));
+            formModel.setBloodLoss(formDataResult.getString("bloodLoss"));
+            formDataList.add(formModel);
+        }
+        return formDataList;
+    }
+
+    @GetMapping(path = "/submitForm")
+    public @ResponseBody String submitForm()
+    {
+        return "Hello World";
+    }
+
     @PostMapping(path = "/submitForm")
     public @ResponseBody void submitForm(formModel formData)
     {
-        System.out.println(formData);
-       /* String submitQuery = "INSERT INTO patientInfo"+
-                "(formID, paramedicName, patientAge, patientGender, patientConsc," +
-                " accidentCategory, accidentDesc, bloodLossRange) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        System.out.println(formData.getParamedicName());
+        String submitQuery = "INSERT INTO patientInfo"+
+                "(paramedicName, patientAge, patientGender, patientConsc," +
+                " accidentCategory, patientDesc, bloodLoss) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?)";
         try
         {
             PreparedStatement submitStatement = connection.prepareStatement(submitQuery);
-            submitStatement.setInt(1, formData.getFormID());
-            submitStatement.setString(2, formData.getParamedicName());
-            submitStatement.setInt(3, formData.getPatientAge());
-            submitStatement.setString(4,formData.getPatientGender());
-            submitStatement.setString(5, formData.getPatientConsc());
-            submitStatement.setString(6, formData.getAccidentCategory());
-            submitStatement.setString(7, formData.getAccidentDesc());
-            submitStatement.setString(8, formData.getBloodLossRange());
+            submitStatement.setString(1, formData.getParamedicName());
+            submitStatement.setInt(2, formData.getPatientAge());
+            submitStatement.setString(3,formData.getPatientGender());
+            submitStatement.setString(4, formData.getPatientConsc());
+            submitStatement.setString(5, formData.getAccidentCategory());
+            submitStatement.setString(6, formData.getPatientDesc());
+            submitStatement.setString(7, formData.getBloodLoss());
             submitStatement.executeUpdate();
             submitStatement.close();
         }
         catch (SQLException submitError)
         {
             submitError.printStackTrace();
-        }*/
+        }
     }
 
 /*    String sql = "update  product  set Name = ?, Prodgroup= ?, Inventory= ?, onorder=? where id=?";
